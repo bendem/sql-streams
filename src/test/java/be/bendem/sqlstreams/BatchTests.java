db4d3aef12afb2074667934b83b1573d7a06e7b8
bendem <online@bendem.be>
2016-05-03 10:17:49 +0200

    Improve batch test

--- src/test/java/be/bendem/sqlstreams/BatchTests.java
+++ src/test/java/be/bendem/sqlstreams/BatchTests.java
@@ -17,7 +17,7 @@ public class BatchTests {
     @Before
     public void setup() throws Exception {
         sql = Sql.connect(DriverManager.getConnection("jdbc:sqlite:"));
-        sql.execute("create table test (a integer)");
+        sql.execute("create table test (a integer primary key autoincrement not null, b integer)");
     }
 
     @After
@@ -27,17 +27,18 @@ public class BatchTests {
 
     @Test
     public void testBatch() {
-        try (PreparedBatchUpdate batch = sql.prepareBatchUpdate("insert into test values(?)")) {
+        try (PreparedBatchUpdate batch = sql.prepareBatchUpdate("insert into test (b) values (?)")) {
             int count = batch
-                .with(1).newBatch()
                 .with(2).newBatch()
                 .with(3).newBatch()
+                .with(4).newBatch()
                 .count();
+
             Assert.assertEquals(3, count);
         }
 
-        try (Stream<Integer> query = sql.query("select * from test order by 1", rs -> rs.getInt(1))) {
-            Assert.assertEquals(Arrays.asList(1, 2, 3), query.collect(Collectors.toList()));
+        try (Stream<Integer> query = sql.query("select b from test order by 1", rs -> rs.getInt(1))) {
+            Assert.assertEquals(Arrays.asList(2, 3, 4), query.collect(Collectors.toList()));
         }
     }
 
