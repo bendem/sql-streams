a0c34a0c4b070a175b29f9ba013fe9fe322d0757
bendem <online@bendem.be>
2017-10-22 20:20:20 +0200

    Actually check the generated value in GeneratedValuesTests

--- src/test/java/be/bendem/sqlstreams/GeneratedValuesTests.java
+++ src/test/java/be/bendem/sqlstreams/GeneratedValuesTests.java
@@ -9,14 +9,27 @@ import java.util.stream.Collectors;
 import static org.junit.Assume.assumeTrue;
 
 public class GeneratedValuesTests extends BaseTests {
+    static class Pair {
+        public final int a;
+        public final int b;
+
+        Pair(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
+    }
+
     @Test
     public void testInsertReturning() throws Exception {
         assumeTrue(database == Database.POSTGRES);
 
-        try (Query query = sql.query("insert into test (b) values (?) returning b").with(1)) {
-            List<Integer> update = query.map(rs -> rs.getInt(1)).collect(Collectors.toList());
+        try (Query query = sql.query("insert into test (b) values (?) returning a, b").with(2)) {
+            List<Pair> update = query
+                .map(rs -> new Pair(rs.getInt(1), rs.getInt(2)))
+                .collect(Collectors.toList());
             Assert.assertEquals(1, update.size());
-            Assert.assertEquals(1, (int) update.get(0));
+            Assert.assertEquals(1, update.get(0).a);
+            Assert.assertEquals(2, update.get(0).b);
         }
     }
 }
